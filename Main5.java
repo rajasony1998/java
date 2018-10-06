500fd7f0d396a060274405ee263f65ece4e7a7a9
rajasony1998 <43901981+rajasony1998@users.noreply.github.com>
2018-10-06 21:54:23 +0530

    Add files via upload

---
+++ Main5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+ class Main
+{
+   public static void main(String args[])
+   {
+      int x, y, z;
+      Scanner in = new Scanner(System.in);
+     
+      x = in.nextInt();
+      y = in.nextInt();
+      z = in.nextInt();
+     
+      if (x > y && x > z)
+         System.out.println(" "+x);
+      else if (y > z)
+         System.out.println(" "+y);
+      else 
+         System.out.println(" "+z);
+     
+   }
+}
