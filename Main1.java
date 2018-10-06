62dd5ea20ff088ad85a73620a75f01873a5a7509
rajasony1998 <43901981+rajasony1998@users.noreply.github.com>
2018-10-06 18:11:37 +0530

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+ class Main1
+{
+  public static void main(String args[])
+  { 
+    int num;
+    Scanner input = new Scanner(System.in);
+    num = input.nextInt();
+    if ( num % 2 == 0 )
+        System.out.println("Entered number is even");
+     else if(num<0)
+     System.out.println("invalid");
+     else
+        System.out.println("Entered number is odd");
+  }
+}
