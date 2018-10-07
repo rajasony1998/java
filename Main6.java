5f73384409b8c291bafa9d07da3eb74f03eb9f77
rajasony1998 <43901981+rajasony1998@users.noreply.github.com>
2018-10-07 12:40:52 +0530

    Add files via upload

---
+++ Main6.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+ class Main
+{
+    public static void main(String args[])
+    {
+       int yr;
+       Scanner scan = new Scanner(System.in);
+       yr = scan.nextInt();
+	   
+       if((yr%4 == 0) && (yr%100!=0))
+       {
+           System.out.print("yes");
+       }
+       else if((yr%100 == 0) && (yr%400 == 0))
+       {
+           System.out.print("yes");
+       }
+       else if(yr%400 == 0)
+       {
+           System.out.print("yes");
+       }
+       else
+       {
+           System.out.print("No");
+       }
+    }
+}
