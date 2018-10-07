1f2e4050f7f9f7e29da9f91d2910632261bf7c52
rajasony1998 <43901981+rajasony1998@users.noreply.github.com>
2018-10-07 20:44:39 +0530

    Update Fact.java

--- Fact.java
+++ Fact.java
@@ -1,15 +1,14 @@
 import java.util.*;
 import java.lang.*;
 public class Factorial{
-      public static void main(String aa[]){
-           int n,p;
+      public static void main(String args[]){
            int f=1;
            Scanner s=new Scanner(System.in);
-           n=s.nextInt();
+            int n=sc.nextInt();
            for(int i=1;i<=n;i++){
                f=f*i;
                
            }
-System.out.printf("%d ",f);
+System.out.println(f);
 }
 }
