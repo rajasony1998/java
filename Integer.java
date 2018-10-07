e28569a6f25e30395294cde7d370b08d03b50cff
rajasony1998 <43901981+rajasony1998@users.noreply.github.com>
2018-10-07 21:14:56 +0530

    Update Integer.java

--- Integer.java
+++ Integer.java
@@ -1,12 +1,16 @@
-import java.io.*;
 import java.util.*;
-class Integer {
-	public static void main (String[] args) 
-	{
-	    Scanner sc=new Scanner(System.in);
-	    int n=sc.nextInt();
-	    String s=Integer.toString(n);
-	    StringBuffer sb=new StringBuffer(s);
-		System.out.println(sb.reverse());
+ class Main {
+
+	public static void main(String[] args) {
+		Scanner x = new Scanner(System.in);
+		String s1 = "";
+		int N = x.nextInt();
+		String s = ""+N;
+		for(int i=s.length()-1;i>=0;i--)
+		{
+			s1 = s1 + s.charAt(i);
+		}
+		System.out.println(s1);
 	}
+
 }
