854b74fb6d5488e99762d22b7b93ea9bce805003
rajasony1998 <43901981+rajasony1998@users.noreply.github.com>
2019-02-11 10:52:37 +0530

    Add files via upload

---
+++ pro11 .java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+ class pro11 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int sum = 0;
+		int n = s.nextInt();
+		int q = s.nextInt();
+		int a[] = new int[n+1];
+		int u[] = new int[q+1];
+		int v[] = new int[q+1];
+		for ( int i = 1 ; i <= n ; i++){
+			a[i] = s.nextInt();
+		}
+		for(int i = 1 ; i <= q ; i++){
+			u[i] = s.nextInt();
+			v[i] = s.nextInt();
+		}
+		
+		for ( int i = 1 ; i <= q ; i++){
+			int min = 9999999;
+			for( int j = u[i]; j<=v[i] ; j++){
+				if(a[j]<min)
+					min = a[j];
+			}
+			System.out.println(min);
+		}
+	}
